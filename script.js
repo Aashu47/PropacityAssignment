c5ab7d7761fd974f2b7eadd280c43351a2023245
Aashu47 <aashu.05134@gmail.com>
2023-08-02 23:21:56 +0530

    updated files

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 
-  const enquireButton = document.getElementById('enquire-button');
-  enquireButton.addEventListener('click', function() {
-  window.location.href = 'popup.html'; // Replace 'login.html' with the actual path to your login page
-  });
-
+ 
 
 
 
