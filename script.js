dce7c3cdfabb02c3f1643852868d9e927f95247b
Aashu47 <aashu.05134@gmail.com>
2023-08-02 09:45:50 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  const enquireButton = document.getElementById('enquire-button');
+  enquireButton.addEventListener('click', function() {
+  window.location.href = 'popup.html'; // Replace 'login.html' with the actual path to your login page
+  });
+
+
+
+
+
+
+
+
